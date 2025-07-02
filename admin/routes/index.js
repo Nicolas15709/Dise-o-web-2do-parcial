a07bf5d234007fc8ab2401ebad5dc9bea385094b
Nicolas <nicolaslopezcadena@hotmail.com>
2025-07-01 19:41:20 -0500

    holaaa

--- admin/routes/index.js
+++ admin/routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
+var axios = require('axios');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-module.exports = router;
+/* GET photos page. */
+ router.get('/photos', async function(req, res, next) {
+ const URL = 'https://dawm-fiec-espol-default-rtdb.firebaseio.com/photos.json'
+ const config = {
+ proxy: {
+ host: 'localhost',
+ port: 4444
+ }
+ }
+ const response = await axios.get(URL) 
+ res.render('fotos', { title: 'Fotos', fotos: response.data });
+ })
+
+
+ module.exports = router;
